557b94ab44ca525b173bb2d15325070a61fb6fdd
akshay07k <akshaykumar5651t@gmail.com>
2024-07-13 14:21:37 +0530

    day1 completed

---
+++ day1.js
@@ -0,0 +1,49 @@
+// // Variable Declaration⬇︎
+// var num1 = 12;
+// console.log(num1);
+
+// let num2 = "string";
+// console.log(num2);
+
+// // Constant Declaration⬇︎
+// const flag = false;
+// console.log(flag);
+
+// // Data Types
+// let number = 123;
+// let string = "abc";
+// let boolean = false;
+// let object = {
+//     name: "xyz",
+//     age: 13
+// }
+// let array = [1,2,3,4,"s"]
+
+// console.log(typeof(number));
+// console.log(typeof(string));
+// console.log(typeof(boolean));
+// console.log(typeof(object));
+// console.log(typeof(array));
+
+// // Reassigning Variables⬇︎
+// let x = 1;
+// console.log(x);
+// x = false;
+// console.log(x);
+
+// // Understanding Const⬇︎
+// const y = 1;
+// console.log(y);
+// y = false;
+// console.log(y);
+
+
+// // Different nature of let & var⬇︎
+// if(true){
+//     console.log(a);
+//     console.log(b);
+//     var a = 12;
+//     let b = 11;
+// }
+// console.log(a);
+// console.log(b);
