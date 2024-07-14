b28ee6137abd0591a3df8c526cdbbc9cabd823cd
akshay07k <akshaykumar5651t@gmail.com>
2024-07-14 20:47:42 +0530

    day2 completed

---
+++ day2.js
@@ -0,0 +1,50 @@
+// // Arithmetic Operations⬇︎
+// let a = 22;
+// let b = 13;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b); //remainder
+
+
+// // Assignment Operators⬇︎
+// let x = 22;
+// let y = 13;
+// x += y; 
+// console.log(x);
+// x -= y; 
+// console.log(x);
+
+
+// // Comparison Operators⬇︎
+// let p = 22;
+// let q = 14;
+// // if(p>q) console.log("p is greater");
+// // if(q<p) console.log("q is lesser");
+// q=22;
+// // console.log(q);
+// // if(p>=q) console.log("p is greater or equal");
+// // if(q<=p) console.log("q is lesser or equal");
+
+// if(p==q) console.log("Both values are equal");
+// if(p===q) console.log("Both values with type are equal");
+
+
+// // Logical Operators⬇︎
+// let t = 12;
+// let w = 32;
+// if(t<15 && w<15){
+//     console.log("Both values are less than 15");
+// }
+// if(t<15 || w<15){
+//     console.log("Either one or both values are less than 15");
+// }
+// if(t!=15){
+//     console.log("t is not 15");
+// }
+
+
+// // Ternary Operator⬇︎
+// let k = 12;
+// k > 0 ? console.log("positive") : console.log("negative");;
