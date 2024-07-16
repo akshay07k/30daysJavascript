deef18c19cd036b0223155184a44652affd5d1f3
akshay07k <akshaykumar5651t@gmail.com>
2024-07-16 21:45:17 +0530

    day4 completed

---
+++ day4.js
@@ -0,0 +1,60 @@
+// // For Loop⬇︎
+// for(let i=1; i<11; i++){
+//     console.log(i);
+// }
+
+// for (let i = 1; i < 11; i++) {
+//     console.log(`5*${i} = ${5*i}`);
+// }
+
+// // While Loop⬇︎
+// let sum = 0, i=1;
+// while(i<11){
+//     sum += i++;
+// }
+// console.log(sum);
+
+// let j = 10;
+// while(j>0){
+//     console.log(j--);
+// }
+
+
+
+// Do While Loop⬇︎
+// let i=1;
+// do{
+//     console.log(i++);
+// }
+// while(i<6)
+
+// let num = 8;
+// let fact = 1;
+// do{
+//     fact *= num--;
+// }while(num>0)
+
+// console.log(fact);
+
+
+
+// // Nested Loops⬇︎
+// for (let i = 1; i <= 5; i++) {
+//     let s = "";
+//     for(let j = 1; j<=i; j++){
+//         s += "* "
+//     }
+//     console.log(s);
+// }
+
+
+// // Loop Control Statements⬇︎
+// for (let i = 1; i < 11; i++) {
+//     if(i==5) continue;
+//     console.log(i);
+// }
+
+// for (let i = 1; i < 11; i++) {
+//     if(i==7) break;
+//     console.log(i);
+// }
