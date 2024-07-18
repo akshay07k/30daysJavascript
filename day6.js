e6873ff15d50c35ee4f1ca0855126e004d9707fb
akshay07k <akshaykumar5651t@gmail.com>
2024-07-18 21:39:39 +0530

    day6 completed

---
+++ day6.js
@@ -0,0 +1,63 @@
+// // Array Creation and Access⬇︎
+// let arr = [1,2,3,4,5]
+// console.log(arr);
+// console.log(arr[0], arr[arr.length-1]);
+
+
+
+// // Array Methods (Basic)⬇︎
+// let arr = [1,2,3,4,5]
+// arr.push(6)
+// console.log(arr);
+
+// arr.pop()
+// console.log(arr);
+
+// arr.shift()
+// console.log(arr);
+
+// arr.unshift(7)
+// console.log(arr);
+
+
+
+// // Array Methods (Intermediate)⬇︎
+// let arr = [1,2,3,4,5]
+// let newArr;
+// newArr = arr.map((element) => 2*element)
+// console.log(newArr);
+
+
+// let arr = [1,2,3,4,5]
+// let newArr;
+// newArr = arr.filter((element) => !(element&1))
+// console.log(newArr);
+
+
+// let arr = [1,2,3,4,5]
+// let sum = 0;
+// sum = arr.reduce((prev, curr) => curr += prev)
+// console.log(sum);
+
+
+
+// // Array Iteration⬇︎
+// let arr = [1,2,3,4,5]
+// for(let i of arr){
+//     console.log(i);
+// }
+
+// arr.forEach((value) => console.log(value))
+
+
+
+// // Multi-dimensional Array⬇︎
+// let toDarr = [[1,2,3],[4,5,6],[7,8,9]]
+// // console.log(toDarr);
+
+// for(let i of toDarr){
+//     // console.log(i);
+//     for(let j of i){
+//         console.log(j);
+//     }
+// }
